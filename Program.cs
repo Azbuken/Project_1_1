ec794d7740f2e90e0250f0928b684ed033f82d23
Aleksey <skitev-aleksey@mail.ru>
2023-04-06 18:54:05 +0300

    add random

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 .. 9
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberA / numberB);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
+
 
