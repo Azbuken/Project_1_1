1d320ad8e8a375e2e4f5c9fd8d02523edd693102
Aleksey <skitev-aleksey@mail.ru>
2023-04-06 19:27:29 +0300

    new project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,2 @@
-﻿Console.WriteLine("Введите имя пользователя: ");
-string username = Console.ReadLine();
-
-if(username.ToLower() == "masha")
-{
-    Console.WriteLine("Ура, это же Masha");
-}
-else
-{
-    Console.WriteLine("Привет, ");
-    Console.WriteLine(username);
-}
-
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
